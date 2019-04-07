c1bf97ec33dfdcb917b6a01672d34834c7f76128
FightingHao <496915484@qq.com>
2019-04-07 14:25:32 +0800

    使用 ES6 WeakMap 模拟栈

---
+++ day01/JS模拟栈_03.js
@@ -0,0 +1,52 @@
+/*
+ * 使用 ES6 WeakMap 模拟栈
+ */
+const items = new WeakMap()
+class Stack {
+  // 初始化栈
+  constructor() {
+    items.set(this, [])
+  }
+
+  // 入栈
+  push(elements) {
+    items.get(this).push(elements)
+  }
+
+  // 出栈
+  pop() {
+    items.get(this).pop()
+  }
+
+  // 返回栈顶元素
+  peek() {
+    return items.get(this)[items.get(this).length - 1]
+  }
+
+  // 判断栈是否为空
+  isEmpty() {
+    return items.get(this).length === 0
+  }
+
+  // 清空栈
+  clear() {
+    items.get(this) = []
+  }
+
+  // 返回栈元素个数
+  size() {
+    return items.get(this).length
+  }
+
+  // 打印栈元素
+  print() {
+    console.log(items.get(this).toString())
+  }
+}
+
+// test
+const stack = new Stack()
+stack.push(1)
+stack.print()
+stack.pop()
+stack.print()
