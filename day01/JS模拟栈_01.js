a06aecbb597aa0f11ae683b9e6b7378107928f99
FightingHao <496915484@qq.com>
2019-04-07 14:25:17 +0800

    使用 ES5 function 模拟栈

---
+++ day01/JS模拟栈_01.js
@@ -0,0 +1,49 @@
+/*
+ * 使用 ES5 function 模拟栈
+ */
+function Stack() {
+  // 初始化栈
+  let items = []
+
+  // 入栈
+  this.push = function (elements) {
+    items.push(elements)
+  }
+
+  // 出栈
+  this.pop = function () {
+    items.pop()
+  }
+
+  // 返回栈顶元素
+  this.peek = function () {
+    return items[items.length - 1]
+  }
+
+  // 判断栈是否为空
+  this.isEmpty = function () {
+    return items.length === 0
+  }
+
+  // 清空栈
+  this.clear = function () {
+    items = []
+  }
+
+  // 返回栈元素个数
+  this.size = function () {
+    return items.length
+  }
+
+  // 打印栈元素
+  this.print = function () {
+    console.log(items.toString())
+  }
+}
+
+// test
+const stack = new Stack()
+stack.push(1)
+stack.print()
+stack.pop()
+stack.print()
